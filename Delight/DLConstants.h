e444cbd624cb9702a1eb13f81b00a923e9a64ef8
Chris Haugli <chris@nowbox.com>
2012-06-15 18:09:06 -0700

    Bump version to 2.1.3.

--- Delight/DLConstants.h
+++ Delight/DLConstants.h
@@ -8,7 +8,7 @@
 
 // Don't change the version here, it is auto-generated via a build script.
 // Change it in Delight-Info.plist instead.
-#define DELIGHT_VERSION @"2.1.2"
+#define DELIGHT_VERSION @"2.1.3"
 
 // The maximum height that OpenGL recordings should have.
 // If a OpenGL backing height is beyond this value, each frame will be scaled down (slow).
