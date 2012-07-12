7b5449ee1598bf9a6cc994e1a80466413a37190c
Chris Haugli <chris@nowbox.com>
2012-07-12 13:26:40 -0700

    Bump version to 2.2.2 and update release notes.

--- Delight/DLConstants.h
+++ Delight/DLConstants.h
@@ -8,8 +8,8 @@
 
 // Don't change the version here, it is auto-generated via a build script.
 // Change it in Delight-Info.plist instead.
-#define DELIGHT_VERSION_PRIVATE @"2.2.1.Private"
-#define DELIGHT_VERSION_PUBLIC @"2.2.1"
+#define DELIGHT_VERSION_PRIVATE @"2.2.2.Private"
+#define DELIGHT_VERSION_PUBLIC @"2.2.2"
 
 #ifdef PRIVATE_FRAMEWORK
     #define DELIGHT_VERSION DELIGHT_VERSION_PRIVATE
